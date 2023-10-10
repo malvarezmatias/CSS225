5c6ec38b8cfc02a8a658e6ea5c664f0adfab7f6a
malvarezmatias <146116137+malvarezmatias@users.noreply.github.com>
2023-10-10 14:29:35 -0500

    Add files via upload

---
+++ Lab 3 Problem 5.py
@@ -0,0 +1,10 @@
+# Martin Alvarez
+# 10/10/23
+
+# This program converts Fahrenheit to Celsius
+
+Fahrenheit = int(input("Enter the temperature in Fahrenheit: "))
+
+Celsius = (Fahrenheit - 32) * 5/9
+
+print('The temperature converts into' , round(Celsius, 2) , "degrees Celsius")
