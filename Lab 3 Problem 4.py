e39ef5a1f457a07cb2ba6d813794b812a441562c
malvarezmatias <146116137+malvarezmatias@users.noreply.github.com>
2023-10-10 14:09:05 -0500

    Add files via upload

---
+++ Lab 3 Problem 4.py
@@ -0,0 +1,11 @@
+# Martin Alvarez
+# 10/10/23
+
+# This program computes the mpg for a car.
+
+Miles = int(input("Enter Miles driven: "))
+Gallons = int(input("Enter Gallons used: "))
+
+MPG =  Miles / Gallons
+
+print("Thank You! Your car's MPG was" , MPG)
