8b415ffbf4b33d485293fc126ecfb8d3c4d0ea90
malvarezmatias <146116137+malvarezmatias@users.noreply.github.com>
2023-10-10 14:15:09 -0500

    Add files via upload

---
+++ Lab 3 Problem 3.py
@@ -0,0 +1,12 @@
+# Martin Alvarez
+# 10/10/23
+
+# This program only lets my instructor and me to be greeted with our name
+print('What is your name')
+name = input()
+
+if name == 'Martin': print("Hello Martin")
+
+elif name == 'Robyn' : print('Hello Robyn')
+
+else: print('Wrong Name')
