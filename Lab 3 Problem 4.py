5ea5ba6fc052a6af2bd7774513f3578d3564ae00
malvarezmatias <146116137+malvarezmatias@users.noreply.github.com>
2023-10-16 17:27:35 -0500

    Add files via upload

---
+++ Lab 3 Problem 4.py
@@ -0,0 +1,10 @@
+# Martin Alvarez
+# 10/16/23
+
+# This program computes the area of a circle
+
+circle = int(input('Please enter the radius of the circle: '))
+
+Area = 3.14 * circle**2
+
+print("Thank You! The area of the circle is", Area )
